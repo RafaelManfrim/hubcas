8f733d1eb66e92e80f776a8033baeb470cc67a10
manfrim <rafaelmanfrim2004@gmail.com>
2024-08-28 13:59:21 -0300

    feat: adicionar Tailwind, ShadcnUI, React Hook Form e Zod ao projeto e configurar logo

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,97 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Input } from "@components/ui/input"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@components/ui/form"
+
+import LogoHubCas from "@assets/logo.png"
+
+const schema = z.object({
+  email: z.string({
+    required_error: "Informe o e-mail do participante",
+    invalid_type_error: "Informe o e-mail do participante"
+  }).email("Informe um e-mail válido"),
+  password: z.string()
+})
+
+type SignInSchema = z.infer<typeof schema>
+
 export function Login() {
+  // const navigate = useNavigate()
+  // const { signIn } = useAuth()
+
+  const form = useForm<SignInSchema>({
+    resolver: zodResolver(schema)
+  })
+
+  function handleSignIn({ email, password }: SignInSchema) {
+    console.log({ email, password })
+    // signIn({ password })
+  }
+
   return (
-    <div>Login</div>
+    <div className="bg-imobverse-background">
+      <div className="mx-auto h-svh flex flex-col justify-center items-center max-w-lg p-4">
+        <a href="/">
+          <img src={LogoHubCas} className="mx-auto w-full max-w-64 mb-8" alt="" />
+        </a>
+        <Form {...form}>
+          <div className="mx-2 w-full">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field: { ...props } }) => (
+                <FormItem className="mb-4">
+                  <FormLabel className="text-white">E-mail do Participante</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-imobverse-input border-0 placeholder:text-gray-400 text-white" 
+                      type="text" 
+                      placeholder="E-mail do Participante" 
+                      {...props} 
+                    />
+                  </FormControl>
+                  <FormMessage>
+                    {form.formState.errors.email?.message}
+                  </FormMessage>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field: { ...props } }) => (
+                <FormItem className="mb-4">
+                  <FormLabel className="text-white">Senha</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="bg-imobverse-input border-0 placeholder:text-gray-400 text-white" 
+                      type="password" 
+                      placeholder="Senha"
+                      {...props} 
+                    />
+                  </FormControl>
+                  <FormMessage>
+                    {form.formState.errors.password?.message}
+                  </FormMessage>
+                </FormItem>
+              )}
+            />
+          </div>
+
+          {/* <CheckoutButton onClick={form.handleSubmit(handleSignIn)}>
+            Entrar
+          </CheckoutButton> */}
+        </Form>
+        {/* <a 
+          className="text-imobverse-emphasis font-semibold mt-4 cursor-pointer hover:brightness-90"
+          onClick={() => navigate("/participant/forgot-my-password/")}
+        >
+          Criar / Esqueci Minha Senha
+        </a> */}
+      </div>
+    </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@utils': path.resolve(__dirname, './src/utils'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@routes': path.resolve(__dirname, './src/routes'),
